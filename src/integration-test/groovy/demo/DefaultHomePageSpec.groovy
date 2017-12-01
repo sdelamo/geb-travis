3b326a9bbf92b8d3e5faec1d69946ad16f2d3714
sdelamo <sergio.delamo@softamo.com>
2017-12-01 15:51:56 +0100

    Initial commit

---
+++ src/integration-test/groovy/demo/DefaultHomePageSpec.groovy
@@ -0,0 +1,19 @@
+package demo
+
+import geb.spock.GebSpec
+import grails.testing.mixin.integration.Integration
+
+@SuppressWarnings('MethodName')
+@Integration
+class DefaultHomePageSpec extends GebSpec {
+
+    def 'verifies there is _<h1>_ header with the text _Welcome to Grails when we visit the home page.'() {
+        when:
+        go '/'
+
+        then:
+
+        $('h1').text() == 'Welcome to Grails'
+
+    }
+}
---
+++ travis-build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Running tests..."
+
+EXIT_STATUS=0
+
+./gradlew check || EXIT_STATUS=$?
+
+exit $EXIT_STATUS
